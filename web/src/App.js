d004b10a773f11f4e896cd94a3af3e2fa52af4d0
amrut <>
2021-09-16 23:40:58 +0800

    Add bootstrap and router

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-import TwitchEmbed from './TwitchEmbed';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import { LinkContainer } from "react-router-bootstrap";
+
+import TwitchEmbed from "./TwitchEmbed";
+
+const Home = () => <span>Home</span>;
+
+const About = () => <span>About</span>;
+
+const Users = () => <span>Users</span>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+      <Router>
+        <Container className="p-3">
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <h2>
+            Current Page is{" "}
+            {/* look through the children <Route>s and render the first one that matches the current URL */}
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route path="/users" component={Users} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </h2>
 
-      <TwitchEmbed
-        id="qtcinderella-1"
-        channel="ludwig" // name of the chat room and channel to stream
-      // video="1144988822" // ID of VOD to play
-      // chat="mobile" // TODO: detect mobile devices
-      />
-    </div>
+          <h2>
+            Navigate to{" "}
+            <ButtonToolbar className="custom-btn-toolbar">
+              <LinkContainer to="/">
+                <Button>Home</Button>
+              </LinkContainer>
+
+              <LinkContainer to="/about">
+                <Button>About</Button>
+              </LinkContainer>
+
+              <LinkContainer to="/users">
+                <Button>Users</Button>
+              </LinkContainer>
+            </ButtonToolbar>
+          </h2>
+        </Container>
+      </Router>
+
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <TwitchEmbed
+          id="qtcinderella-1"
+          channel="ludwig" // name of the chat room and channel to stream
+          // video="1144988822" // ID of VOD to play
+          // chat="mobile" // TODO: detect mobile devices
+        />
+      </div>
+    </React.Fragment>
   );
 }
 
