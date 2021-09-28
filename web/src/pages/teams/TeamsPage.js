f8a1e00190079cba49d2c753053c9e37fde784c6
amrut <>
2021-09-28 07:26:54 -0700

    Add team results

--- web/src/pages/teams/TeamsPage.js
+++ web/src/pages/teams/TeamsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Container, Col, Row } from "react-bootstrap";
+import { Card, Container, Col, Row, ListGroup } from "react-bootstrap";
 
 import { getTeamsInfo } from "apis";
 
@@ -13,6 +13,7 @@ class TeamsPage extends React.PureComponent {
     this.state = {
       isLoading: true,
       teams: [],
+      teamResults: [],
     };
   }
 
@@ -21,10 +22,11 @@ class TeamsPage extends React.PureComponent {
     if (ret.error != null) {
       console.error(ret.error);
     } else {
-      const { teams } = ret.resp;
+      const { teams, team_results } = ret.resp;
 
       this.setState({
         teams: teams,
+        teamResults: team_results,
       });
     }
 
@@ -34,7 +36,7 @@ class TeamsPage extends React.PureComponent {
   }
 
   render() {
-    const { isLoading, teams } = this.state;
+    const { isLoading, teams, teamResults } = this.state;
 
     return (
       <Container className="teams-page">
@@ -45,50 +47,87 @@ class TeamsPage extends React.PureComponent {
         ) : teams.length === 0 ? (
           <h5>Teams are not available</h5>
         ) : (
-          <Row className="g-4">
-            {teams.map((team) => (
-              <Col xs={12} sm={6} lg={4}>
-                <Card>
-                  <Card.Header as="h5">{team.name}</Card.Header>
-                  <Card.Img
-                    variant="top"
-                    alt="thumbnail"
-                    src={team.thumbnail_url}
-                  />
-                  <Card.Body>
-                    <Card.Text>
-                      <ul className="no-bullets">
-                        <li>
-                          <Card.Subtitle>
-                            <a
-                              href={`https://www.twitch.tv/${team.captain}`}
-                              target="_blank"
-                              rel="noreferrer"
-                              className="link"
-                            >
-                              {team.captain} (captain)
-                            </a>
-                          </Card.Subtitle>
-                        </li>
-                        {team.users.map((user) => (
+          <React.Fragment>
+            <Row className="g-4">
+              {teams.map((team) => (
+                <Col xs={12} sm={6} lg={4}>
+                  <Card>
+                    <Card.Header as="h5">{team.name}</Card.Header>
+                    <Card.Img
+                      variant="top"
+                      alt="thumbnail"
+                      src={team.thumbnail_url}
+                    />
+                    <Card.Body>
+                      <Card.Text>
+                        <ul className="no-bullets">
                           <li>
-                            <a
-                              href={`https://www.twitch.tv/${user}`}
+                            <Card.Subtitle>
+                              <a
+                                href={`https://www.twitch.tv/${team.captain}`}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="link"
+                              >
+                                {team.captain} (captain)
+                              </a>
+                            </Card.Subtitle>
+                          </li>
+                          {team.users.map((user) => (
+                            <li>
+                              <a
+                                href={`https://www.twitch.tv/${user}`}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="link"
+                              >
+                                {user}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+
+            <br />
+            <h4>Results</h4>
+            <Row className="g-4">
+              {teamResults.map((teamResult) => (
+                <Col xs={12} sm={6} lg={4}>
+                  <Card>
+                    <Card.Header as="h5">{teamResult.team_name}</Card.Header>
+                    <Card.Body>
+                      <Card.Subtitle className="mb-2">
+                        Total points: {teamResult.total_points}
+                      </Card.Subtitle>
+                      <Card.Text>
+                        {teamResult.results.map((result) => (
+                          <ListGroup>
+                            <ListGroup.Item
+                              variant="warning"
+                              action
+                              href={result.vod_url}
                               target="_blank"
                               rel="noreferrer"
-                              className="link"
                             >
-                              {user}
-                            </a>
-                          </li>
+                              <b>{result.contest_name}</b>: {result.points}{" "}
+                              points
+                              <br />
+                              {result.description}
+                            </ListGroup.Item>
+                          </ListGroup>
                         ))}
-                      </ul>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </React.Fragment>
         )}
       </Container>
     );
